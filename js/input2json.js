08bd5133662c267b974d999c318bdcdde9ddfd21
Frank Zappulla <frank.zappulla@jax.org>
2019-09-26 22:14:02 -0400

    first commit

---
+++ js/input2json.js
@@ -0,0 +1,23 @@
+/*javascript for generating JSON from form input*/
+
+/**
+ * A handler function to prevent default submission and run our custom script.
+ * @param  {Event} event  the submit event triggered by the user
+ * @return {void}
+ */
+const handleFormSubmit = event => {
+
+  // Stop the form from submitting since we’re handling that with AJAX.
+  event.preventDefault();
+
+  // TODO: Call our function to get the form data.
+  const data = {};
+
+  // Demo only: print the form data onscreen as a formatted JSON object.
+  const dataContainer = document.getElementsByClassName('results__display')[0];
+
+  // Use `JSON.stringify()` to make the output valid, human-readable JSON.
+  dataContainer.textContent = JSON.stringify(data, null, "  ");
+
+  // ...this is where we’d actually do something with the form data...
+};
