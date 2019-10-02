b18f6bd9e4feaede2ed53c7e17e7c2f80bb238f0
zappuf <frank.zappulla@jax.org>
2019-10-02 14:01:19 -0400

    enhanced timestamp

--- js/input2json.js
+++ js/input2json.js
@@ -110,17 +110,60 @@ function download(filename, text) {
     document.body.removeChild(element);
 }
 
+//*** This code is copyright 2002-2016 by Gavin Kistner, !@phrogz.net
+//*** It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
+//*** Reuse or modification is free provided you abide by the terms of that license.
+//*** (Including the first two lines above in your source code satisfies the conditions.)
+
+// Include this code (with notice above ;) in your library;
+
+Date.prototype.customFormat = function(formatString){
+	var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,hhhh,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
+	var dateObject = this;
+	YY = ((YYYY=dateObject.getFullYear())+"").slice(-2);
+	MM = (M=dateObject.getMonth()+1)<10?('0'+M):M;
+	MMM = (MMMM=["January","February","March","April","May","June","July","August","September","October","November","December"][M-1]).substring(0,3);
+	DD = (D=dateObject.getDate())<10?('0'+D):D;
+	DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dateObject.getDay()]).substring(0,3);
+	th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
+	formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#th#",th);
+
+	h=(hhh=dateObject.getHours());
+	if (h==0) h=24;
+	if (h>12) h-=12;
+	hh = h<10?('0'+h):h;
+  hhhh = hhh<10?('0'+hhh):hhh;
+	AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
+	mm=(m=dateObject.getMinutes())<10?('0'+m):m;
+	ss=(s=dateObject.getSeconds())<10?('0'+s):s;
+	return formatString.replace("#hhhh#",hhhh).replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
+}
+
+
+function getCurrentDate(){
+  var currentDate = new Date();
+  var year = currentDate.getFullYear();
+  var month = currentDate.getMonth();
+  var date = currentDate.getDate();
+  var timestamp = currentDate.getTime();
+  return year + "-" + (month+1) + "-" + date //+ "-" + timestamp;
+}
+
+
+function getPrettyTime(){
+  var time = new Date();
+  return time.customFormat("#hhh#-#mm#-#ss#");
+}
+
+
 // Start file download.
 document.getElementById("dwn-btn").addEventListener("click", function(){
-    var currentDate = new Date();
-    var year = currentDate.getFullYear();
-    var month = currentDate.getMonth();
-    var date = currentDate.getDate();
-    var timestamp = currentDate.getTime();
+    var time = new Date(time);
     var action = document.getElementById("actionType").value;
     // Generate download file with some content
     var text = document.getElementById("text-val").value;
-    var filename = year + "-" + (month+1) + "-" + date + "-" + timestamp + "_" + action + ".json";
+    // var filename = year + "-" + (month+1) + "-" + date + "-" + time + "_" + action + ".json";
+    var filename = getCurrentDate() + "_" + getPrettyTime() + "_" + action + ".json";
 
     download(filename, text);
 }, false);
